a08cb1cf0280b6f4799fdb7211df1b4bed3f62a8
jchanho99 <jchanho99@gmail.com>
2025-05-20 21:45:08 +0900

    변수 이름 'leafresh-'로 변경

--- terraform/v2-docker/modules/network/main.tf
+++ terraform/v2-docker/modules/network/main.tf
@@ -2,13 +2,13 @@
 
 resource "google_compute_network" "vpc" {
   project                 = var.project_id
-  name                    = "three-tier-vpc"
+  name                    = "leafresh-vpc"
   auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "nextjs_public_subnet" {
   project       = var.project_id
-  name          = "nextjs-public-subnet"
+  name          = "leafresh-public-subnet-fe"
   ip_cidr_range = var.nextjs_subnet_cidr
   region        = var.region
   network       = google_compute_network.vpc.self_link
@@ -16,7 +16,7 @@ resource "google_compute_subnetwork" "nextjs_public_subnet" {
 
 resource "google_compute_subnetwork" "springboot_private_subnet" {
   project       = var.project_id
-  name          = "springboot-private-subnet"
+  name          = "leafresh-private-subnet-be"
   ip_cidr_range = var.springboot_subnet_cidr
   region        = var.region
   network       = google_compute_network.vpc.self_link
@@ -24,28 +24,8 @@ resource "google_compute_subnetwork" "springboot_private_subnet" {
 
 resource "google_compute_subnetwork" "db_private_subnet" {
   project       = var.project_id
-  name          = "db-private-subnet"
+  name          = "leafresh-private-subnet-db"
   ip_cidr_range = var.db_subnet_cidr
   region        = var.region
   network       = google_compute_network.vpc.self_link
-}
-
-output "vpc_name" {
-  description = "생성된 VPC의 이름"
-  value       = google_compute_network.vpc.name
-}
-
-output "nextjs_subnet_self_link" {
-  description = "Next.js Public Subnet의 Self Link"
-  value       = google_compute_subnetwork.nextjs_public_subnet.self_link
-}
-
-output "springboot_subnet_self_link" {
-  description = "Spring Boot Private Subnet의 Self Link"
-  value       = google_compute_subnetwork.springboot_private_subnet.self_link
-}
-
-output "db_subnet_self_link" {
-  description = "MySQL/Redis Private Subnet의 Self Link"
-  value       = google_compute_subnetwork.db_private_subnet.self_link
 }
