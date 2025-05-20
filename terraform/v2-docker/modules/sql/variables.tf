0549e23a32835b1bf486c98638b729c5a8dd9075
pieceofizzy <jaiunreve2@gmail.com>
2025-05-20 18:37:40 +0900

    DB 파일 추가

--- terraform/v2-docker/modules/sql/variables.tf
+++ terraform/v2-docker/modules/sql/variables.tf
@@ -1,24 +1,50 @@
 variable "project_id" {
-  type = string
-  description = "GCP Project ID"
+  description = "GCP 프로젝트 ID"
+  type        = string
 }
 
 variable "region" {
-  type = string
-  description = "GCP Region"
+  description = "GCP 리전"
+  default     = "asia-northeast3"
 }
 
-variable "private_db_subnet_id" {
-  type = string
-  description = "Private Subnet (DB) ID"
+variable "zone" {
+  description = "GCP 존"
+  default     = "asia-northeast3-a"
 }
 
-variable "mysql_instance_name" {
-  type = string
-  description = "MySQL 인스턴스 이름"
+variable "instance_name" {
+  description = "인스턴스 이름"
+  default     = "leafresh-gce-db"
 }
 
-variable "tags" {
-  type = map(string)
-  description = "리소스 태그"
+variable "machine_type" {
+  description = "커스텀 머신 타입 (e2-custom-1-3072: 1vCPU, 3GB RAM)"
+  default     = "e2-custom-1-3072"
+}
+
+variable "network" {
+  description = "VPC 네트워크 이름"
+  type        = string
+}
+
+variable "subnetwork" {
+  description = "DB 서브넷 이름"
+  type        = string
+}
+
+variable "service_account_email" {
+  description = "서비스 계정 이메일"
+  type        = string
+}
+
+variable "mysql_root_password" {
+  description = "MySQL root 비밀번호"
+  type        = string
+  sensitive   = true
+}
+
+variable "mysql_database" {
+  description = "MySQL 초기 DB 이름"
+  default     = "leafresh"
 }
